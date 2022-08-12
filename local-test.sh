be89d733da445851411564cae1357eee169e6746
Henry Will <99243374+henry-will@users.noreply.github.com>
2022-08-12 22:05:48 +0900

    Add a ERC20 test

--- local-test.sh
+++ local-test.sh
@@ -3,14 +3,14 @@ DOCKER_COMPOSE_ENV_FILE=./env/.env-new-new
 
 set -e # exit on first error
 
-cd servicechain-docker
-
 echo ""
 echo "------------------------------------------------------"
 echo "Start containers"
 echo "------------------------------------------------------"
 echo ""
+cd servicechain-docker
 docker-compose -f docker-compose-servicechain.yml --env-file $DOCKER_COMPOSE_ENV_FILE up -d
+cd ..
 
 echo "Sleep 50 seconds because scn node needs to warm up during about 30 seconds"
 sleep 50
@@ -20,24 +20,44 @@ echo "------------------------------------------------------"
 echo "Check EN Operator"
 echo "------------------------------------------------------"
 echo ""
+cd servicechain-docker
 docker-compose -f docker-compose-servicechain.yml --env-file $DOCKER_COMPOSE_ENV_FILE exec -T EN-0 cat /klaytn/log/init.log
+cd ..
 
 echo ""
 echo "------------------------------------------------------"
 echo "Check SCN Operator"
 echo "------------------------------------------------------"
+cd servicechain-docker
 docker-compose -f docker-compose-servicechain.yml --env-file $DOCKER_COMPOSE_ENV_FILE exec -T SCN-0 cat /klaytn/log/init.log
+cd ..
 
 echo ""
 echo "------------------------------------------------------"
 echo "Make bridge-info.json"
 echo "------------------------------------------------------"
 echo ""
+cd servicechain-docker
 sh bridge-info/default-bridge-info.sh
+cd ..
+
+echo ""
+echo "------------------------------------------------------"
+echo "Test the ERC20 value transfer"
+echo "------------------------------------------------------"
+echo ""
+
+cp servicechain-docker/bridge-info/bridge-info.json tests/value-transfer/config
+cd tests/value-transfer
+npm i
+cd ERC20 && bash run_testcase.sh
+cd ../../..
 
 echo ""
 echo "------------------------------------------------------"
 echo "Stop containers"
 echo "------------------------------------------------------"
 echo ""
+cd servicechain-docker
 docker-compose -f docker-compose-servicechain.yml --env-file $DOCKER_COMPOSE_ENV_FILE down
+cd ..
---
+++ tests/value-transfer/ERC20/ERC20-transfer-1step.js
@@ -0,0 +1,84 @@
+const Caver = require('caver-js');
+const fs = require('fs');
+
+const conf = JSON.parse(fs.readFileSync(process.env.transferConfig, 'utf8'));
+
+const childBuildPath = process.env.childBuildPath || '../build'
+const parentBuildPath = process.env.parentBuildPath || '../build'
+const childTokenAbi = JSON.parse(fs.readFileSync(`${childBuildPath}/ServiceChainToken.abi`, 'utf8'));
+const parentTokenAbi = JSON.parse(fs.readFileSync(`${parentBuildPath}/ServiceChainToken.abi`, 'utf8'));
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function parentTokenTransfer() {
+  const testcase = process.argv[1].substring(process.argv[1].lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "");
+  console.log(`------------------------- parent sent to bob ${testcase} START -------------------------`)
+  const scnCaver = new Caver(conf.url.child);
+  const scnInstance = new scnCaver.klay.Contract(childTokenAbi, conf.contract.child.token);
+
+  const enCaver = new Caver(conf.url.parent);
+  const enInstance = new enCaver.klay.Contract(parentTokenAbi, conf.contract.parent.token);
+
+  conf.sender.child.address = scnCaver.klay.accounts.wallet.add(conf.sender.child.key).address;
+  conf.sender.parent.address = enCaver.klay.accounts.wallet.add(conf.sender.parent.key).address;
+  const bob = "0xd40b6909eb7085590e1c26cb3becc25368e249e9";
+  
+  try {
+    let balance = await scnInstance.methods.balanceOf(bob).call();
+    console.log("bob balance:", balance);
+
+    // Transfer main chain to service chain
+    console.log("requestValueTransfer..")
+    await enInstance.methods.requestValueTransfer(100, bob, 0, []).send({from:conf.sender.parent.address, gas: 1000000});
+    // Wait event to be trasnferred to child chain and contained into new block
+    await sleep(6000);
+
+    // Check bob balance in Service Chain
+    balance = await scnInstance.methods.balanceOf(bob).call();
+    console.log("bob balance:", balance);
+  } catch (e) {
+    console.log("Error:", e);
+    process.exit(1);
+  }
+  console.log(`------------------------- ${testcase} END -------------------------`)
+}
+
+async function childTokenTransfer() {
+  const testcase = process.argv[1].substring(process.argv[1].lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "");
+  console.log(`------------------------- child send to alice ${testcase} START -------------------------`)
+  const scnCaver = new Caver(conf.url.child);
+  const scnInstance = new scnCaver.klay.Contract(childTokenAbi, conf.contract.child.token);
+
+  const enCaver = new Caver(conf.url.parent);
+  const enInstance = new enCaver.klay.Contract(parentTokenAbi, conf.contract.parent.token);
+
+  conf.sender.child.address = scnCaver.klay.accounts.wallet.add(conf.sender.child.key).address;
+  conf.sender.parent.address = enCaver.klay.accounts.wallet.add(conf.sender.parent.key).address;
+  const alice = "0xc40b6909eb7085590e1c26cb3becc25368e249e9";
+
+  try {
+    let balance = await enInstance.methods.balanceOf(alice).call();
+    console.log("alice balance:", balance);
+
+    // Transfer main chain to service chain
+    console.log("requestValueTransfer..")
+    await scnInstance.methods.requestValueTransfer(100, alice, 0, []).send({from:conf.sender.child.address, gas: 1000000});
+    // Wait event to be transferred to child chain and contained into new block
+    await sleep(6000);
+
+    // Check alice balance in Service Chain
+    balance = await enInstance.methods.balanceOf(alice).call();
+    console.log("alice balance:", balance);
+  } catch (e) {
+    console.log("Error:", e);
+    process.exit(1);
+  }
+  console.log(`------------------------- ${testcase} END -------------------------`)
+}
+
+(async function testErc20ValueTransfer() {
+  await parentTokenTransfer()
+  await childTokenTransfer()
+})()
